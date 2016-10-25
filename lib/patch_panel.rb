16b4b9323e4edc9d80bbb7f770837d81ca787396
Tomoki Imai <t-imai@ist.osaka-u.ac.jp>
2016-10-25 20:39:34 +0900

    レポートを追加

--- lib/patch_panel.rb
+++ lib/patch_panel.rb
@@ -1,7 +1,8 @@
 # Software patch-panel.
 class PatchPanel < Trema::Controller
   def start(_args)
-    @patch = Hash.new { [] }
+    @patch = Hash.new {|h,k| h[k] = []  }
+    @mirroring = Hash.new {|h,k| h[k] = []  }
     logger.info 'PatchPanel started.'
   end
 
@@ -14,12 +15,25 @@ class PatchPanel < Trema::Controller
 
   def create_patch(dpid, port_a, port_b)
     add_flow_entries dpid, port_a, port_b
-    @patch[dpid] += [port_a, port_b].sort
+    @patch[dpid] << [port_a, port_b].sort
   end
 
   def delete_patch(dpid, port_a, port_b)
+    return "No such patch\n" if @patch[dpid].delete([port_a, port_b].sort).nil?
     delete_flow_entries dpid, port_a, port_b
-    @patch[dpid] -= [port_a, port_b].sort
+    return ""
+  end
+
+  def create_mirroring(dpid, port, mirror_port)
+    @mirroring[dpid] << [port,mirror_port]
+    create_mirror_flow_entries dpid,port,mirror_port
+  end
+
+  def print_port(dpid)
+    array = Array.new()
+    array << @patch
+    array << @mirroring
+    return array
   end
 
   private
@@ -36,5 +50,25 @@ class PatchPanel < Trema::Controller
   def delete_flow_entries(dpid, port_a, port_b)
     send_flow_mod_delete(dpid, match: Match.new(in_port: port_a))
     send_flow_mod_delete(dpid, match: Match.new(in_port: port_b))
+  end
+
+  def create_mirror_flow_entries(dpid, port, mirror)
+    port_pair = nil
+    @patch[dpid].each do |port_a, port_b|
+      if port_a == port then
+        port_pair = port_b
+      elsif port_b == port then
+        port_pair = port_a
+      end
+    end
+
+    send_flow_mod_delete(dpid, match: Match.new(in_port: port))
+    send_flow_mod_delete(dpid, match: Match.new(in_port: port_pair)) if ! port_pair.nil?
+    send_flow_mod_add(dpid,
+                      match: Match.new(in_port: port),
+                      actions: [SendOutPort.new(port_pair), SendOutPort.new(mirror),])
+    send_flow_mod_add(dpid,
+                      match: Match.new(in_port: port_pair),
+                      actions: [SendOutPort.new(port), SendOutPort.new(mirror),]) if ! port_pair.nil?
   end
 end
